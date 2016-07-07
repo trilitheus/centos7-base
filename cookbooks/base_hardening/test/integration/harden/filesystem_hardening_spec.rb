6b40e9dae07bfb283211e29a16ca7cae899a8805
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-07-07 15:04:36 +0100

    Rename base hardening tests to filesytem hardening

--- cookbooks/base_hardening/test/integration/harden/filesystem_hardening_spec.rb
+++ cookbooks/base_hardening/test/integration/harden/filesystem_hardening_spec.rb
@@ -1,4 +1,4 @@
-control 'cis-1.1.1' do
+control 'cis-fs-1.1.1' do
   impact 1.0
   title 'Filesystem: Use separate partition for /tmp'
   desc "Since the /tmp directory is intended to be world-writable, there is a risk of resource
@@ -16,7 +16,7 @@ control 'cis-1.1.1' do
   end
 end
 
-control 'cis-1.1.2' do
+control 'cis-fs-1.1.2' do
   impact 1.0
   title 'Filesystem: Set nodev option for /tmp'
   desc "Since the /tmp filesystem is not intended to support devices, set this option to ensure that
@@ -31,7 +31,7 @@ control 'cis-1.1.2' do
   end
 end
 
-control 'cis-1.1.3' do
+control 'cis-fs-1.1.3' do
   impact 1.0
   title 'Filesystem: Set nosuid option for /tmp'
   desc "Since the /tmp filesystem is only intended for temporary file storage, set this option to
@@ -46,7 +46,7 @@ control 'cis-1.1.3' do
   end
 end
 
-control 'cis-1.1.4' do
+control 'cis-fs-1.1.4' do
   impact 1.0
   title 'Filesystem: Set noexec option for /tmp'
   desc "Since the /tmp filesystem is only intended for temporary file storage, set this option to
@@ -61,7 +61,7 @@ control 'cis-1.1.4' do
   end
 end
 
-control 'cis-1.1.5' do
+control 'cis-fs-1.1.5' do
   impact 1.0
   title 'Filesystem: Use separate partition for /var'
   desc "Since the /var directory may contain world-writable files and directories, there is a risk of
@@ -73,10 +73,10 @@ control 'cis-1.1.5' do
   end
 end
 
-# control 'cis-1.1.6' do
+# control 'cis-fs-1.1.6' do
 #   title 'Filesystem: Bind Mount the /var/tmp directory to /tmp'
 
-control 'cis-1.1.7' do
+control 'cis-fs-1.1.7' do
   impact 1.0
   title 'Filesystem: Create Separate Partition for /var/log'
   desc "There are two important reasons to ensure that system logs are stored on a separate
@@ -89,7 +89,7 @@ control 'cis-1.1.7' do
   end
 end
 
-control 'cis-1.1.8' do
+control 'cis-fs-1.1.8' do
   impact 1.0
   title 'Filesystem: Create Separate Partition for /var/log/audit'
   desc "There are two important reasons to ensure that data gathered by auditd is stored on a
@@ -104,7 +104,7 @@ control 'cis-1.1.8' do
   end
 end
 
-control 'cis-1.1.9' do
+control 'cis-fs-1.1.9' do
   impact 1.0
   title 'Filesystem: Create Separate Partition for /home'
   desc "If the system is intended to support local users, create a separate partition for the /home
@@ -117,7 +117,7 @@ control 'cis-1.1.9' do
   end
 end
 
-control 'cis-1.1.10' do
+control 'cis-fs-1.1.10' do
   impact 1.0
   title 'Filesystem: Set nodev option for /home'
   desc "Since the user partitions are not intended to support devices, set this option to ensure that
@@ -132,25 +132,25 @@ control 'cis-1.1.10' do
   end
 end
 
-#control 'cis-1.1.11' do
+#control 'cis-fs-1.1.11' do
 #  title 'Filesystem: Add nodev Option to Removable Media Partitions'
 
-#control 'cis-1.1.12' do
+#control 'cis-fs-1.1.12' do
 #  title 'Filesystem: Add noexec Option to Removable Media Partitions'
 
-#control 'cis-1.1.13' do
+#control 'cis-fs-1.1.13' do
 #  title 'Filesystem: Add nosuid Option to Removable Media Partitions'
 
-#control 'cis-1.1.14' do
+#control 'cis-fs-1.1.14' do
 #  title 'Filesystem: Add nodev Option to /dev/shm Partition'
 
-#control 'cis-1.1.15' do
+#control 'cis-fs-1.1.15' do
 #  title 'Filesystem: Add nosuid Option to /dev/shm Partition'
 
-#control 'cis-1.1.16' do
+#control 'cis-fs-1.1.16' do
 #  title 'Filesystem: Add noexec Option to /dev/shm Partition'
 
-control 'cis-1.1.17' do
+control 'cis-fs-1.1.17' do
   impact 1.0
   title 'Set Sticky Bit on All World-Writable Directories'
   desc "This feature prevents the ability to delete or rename files in world writable directories
@@ -163,7 +163,7 @@ control 'cis-1.1.17' do
   end
 end
 
-control 'cis-1.1.18' do
+control 'cis-fs-1.1.18' do
   impact 0.1
   title 'Disable Mounting of cramfs Filesystems'
   desc "Removing support for unneeded filesystem types reduces the local attack surface of the
@@ -179,16 +179,46 @@ control 'cis-1.1.18' do
   end
 end
 
-#control 'cis-1.1.19' do
+#control 'cis-fs-1.1.19' do
 #  title 'Disable Mounting of freevxfs Filesystems'
 
-#control 'cis-1.1.20' do
+#control 'cis-fs-1.1.20' do
 #  title 'Disable Mounting of jffs2 Filesystems'
 
-#control 'cis-1.1.21' do
+#control 'cis-fs-1.1.21' do
 #  title 'Disable Mounting of hfs Filesystems'
 
-#control 'cis-1.1.21' do
+#control 'cis-fs-1.1.22' do
 #  title 'Disable Mounting of hfsplus Filesystems'
 
+control 'cis-fs-1.1.23' do
+  impact 0.1
+  title 'Disable Mounting of squashfs Filesystems'
+  desc "Removing support for unneeded filesystem types reduces the local attack surface of the
+        server. If this filesystem type is not needed, disable it."
+  tag kernel: 'squashfs'
 
+  describe kernel_module('squashfs') do
+    it { should_not be_loaded }
+  end
+  check_squashfs = command('/sbin/modprobe -n -v squashfs')
+  describe check_squashfs do
+    its('stdout') { should eq 'install /bin/true' }
+  end
+end
+
+control 'cis-fs-1.1.24' do
+  impact 0.1
+  title 'Disable Mounting of udf Filesystems'
+  desc "Removing support for unneeded filesystem types reduces the local attack surface of the
+        server. If this filesystem type is not needed, disable it."
+  tag kernel: 'udf'
+
+  describe kernel_module('udf') do
+    it { should_not be_loaded }
+  end
+  check_udf = command('/sbin/modprobe -n -v udf')
+  describe check_udf do
+    its('stdout') { should eq 'install /bin/true' }
+  end
+end
